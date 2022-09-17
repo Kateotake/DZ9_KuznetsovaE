89eb680d1db8ec1feff224d0859e196eecc56288
KuznetsovaE <kettt56@mail.ru>
2022-09-17 23:21:35 +0300

    dz9

---
+++ Zadacha64/Zadacha64.cs
@@ -0,0 +1,14 @@
+﻿//Задача 64: Задайте значения M и N. 
+//Напишите программу, которая выведет все натуральные 
+//числа в промежутке от M до N.
+// M = 1; N = 5. -> ""1, 2, 3, 4, 5""
+// M = 4; N = 8. -> ""4, 6, 7, 8""
+
+
+string NumbersRec(int M, int N)
+{
+    if (M <= N) return $"{M} " + NumbersRec(M + 1, N);
+    else return String.Empty;
+}
+
+Console.WriteLine(NumbersRec(10, 20));
---
+++ Zadacha68/Zadacha68.cs
@@ -0,0 +1,12 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана 
+//с помощью рекурсии. Даны два неотрицательных числа m и n.
+
+// m = 3, n = 2 -> A(m,n) = 29
+int Acc(int n, int m)
+{
+    if (n == 0) return m + 1;
+    else if ((n != 0) && (m == 0)) return Acc(n - 1, 1);
+    else return Acc(n - 1, Acc(n, m - 1));
+}
+
+Console.WriteLine (Acc(3,2));
